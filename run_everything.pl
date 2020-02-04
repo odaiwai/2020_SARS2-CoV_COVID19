9b1104b241219c963976d557f4795bdcdcc68594
Dave O'Brien <github@diaspoir.net>
2020-02-04 10:39:57 +0800

    added script to run everything

--- run_everything.pl
+++ run_everything.pl
@@ -13,25 +13,30 @@ use warnings;
 use utf8;
 
 my $verbose = 1;
+my $options = "silent";
 my @getters = `ls get_*.p?`;
 my @parsers = `ls process_*.p?`;	
 
 while (my $arg = shift(@ARGV)) {
-	if ( $arg =~ /verbose/ ) {
-		$verbose = 1;
-	}
+	if ( $arg =~ /verbose/ ) { $verbose = 1; }
+	if ( $arg =~ /silent/ )  { $verbose = 0; }
+}
+if ( $verbose ) {
+	$options = "verbose";
 }
 
+
 print run_all_scripts(@getters);
-print run_all_scripts(@parsers);
+#print run_all_scripts(@parsers);
 
 ## end
 sub run_all_scripts {
 	my @scripts = @_;
 	my $results = 0;
-	for my $scrip (@scripts) {
+	for my $script (@scripts) {
 		chomp $script;
-		my $result = `./$script`;
+		print "Running $script $options...\n" if $verbose;
+		my $result = `./$script $options`;
 		print "$result\n" if $verbose;
 		$results ++;
 	}
