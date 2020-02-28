2bb6e8da09dd2e183f50a7733cd8c7c0e573ad41
Dave O'Brien <github@diaspoir.net>
2020-02-28 23:29:12 +0800

    refining the processing of the nCorv data from China

--- process_ncor_2019_data.py
+++ process_ncor_2019_data.py
@@ -7,24 +7,61 @@ dave o'brien (c) 2020
 
 CC: BY-SA
 """
-import sys, os, re
+import sys
+import os
+import re
 import sqlite3
 import json
-#import openpyxl
 import datetime
-from db_helper import *
+from db_helper import * # This is a library of my own database routines - it just 
+                        # wraps sqlite commands into handier methods I like
 import matplotlib.pyplot as plt
 #import pandas as pd
 #import numpy as np
 
 def make_tables():
-    # Make the Database tables
+    # Make the Database tables from the JSON
+    """ 
+        A province looks like this:
+            "provinceShortName":"湖北",
+            "comment":"湖北省卫健委尚未发布昨日确诊数据，武汉确诊数据及治愈死亡数据来自国家卫健委。",
+            "deadCount":2250,
+            "confirmedCount":63454,
+            "provinceName":"湖北省",
+            "curedCount":13557,
+            "suspectedCount":0,
+            "locationId":420000"
+        A City looks like this:
+            "curedCount":222,
+            "suspectedCount":0,
+            "cityName":"深圳",
+            "locationId":440300,
+            "deadCount":2,
+            "confirmedCount":417,       # Total of Confirmed, Dead, Cured
+            "currentConfirmedCount":193
+    """
+
     tabledefs = {
-        'hksarg_pr': 'timestamp text Unique Primary Key, New Int, Total Int, Cured Int, Remain Int, Stable Int, Serious Int, Critical Int, Confirmed Int, Dead Int',
-        '3g_dxy_cn_province': 'UUID text unique Primary Key, Timestamp Int, ISO_Date Text, Province_ZH Text, Province_EN Text, Confirmed Int, Suspected Int, Dead Int, Cured Int, Comment Text',
-        '3g_dxy_cn_city': 'UUID text unique Primary Key, Timestamp Int, ISO_Date Text, Province_ZH Text, Province_EN Text, City_ZH Text, City_EN, Confirmed Int, Suspected Int, Dead Int, Cured Int',
-        'jhu': 'UUID text unique Primary Key, Timestamp Int, Country Text, Province Text, Confirmed Int, Dead Int, Cured Int',
-        'china_places': 'OBJECTID Int UNIQUE Primary Key, ADMIN_TYPE Text, ADM2_CAP Text, ADM2_EN Text, ADM2_ZH Text, ADM2_PCODE Text, ADM1_EN Text, ADM1_ZH Text, ADM1_PCODE Text, ADM0_EN Text, ADM0_ZH Text, ADM0_PCODE Text'
+        'hksarg' : ('timestamp text Unique Primary Key, New Integer, Total Integer, '
+                    'Cured Integer, Remain Integer, Stable Integer, Serious Integer, '
+                    'Critical Integer, Confirmed Integer, Dead Integer'),
+        'cn_prov': ('UUID text unique Primary Key, Timestamp Integer, ISO_Date Text, '
+                    'ProvinceName Text, Province_EN Text, confirmedCount Integer, '
+                    'suspectedCount Integer, deadCount Integer, curedCount Integer, '
+                    'LocationID Integer, Comment Text, provinceShortName Text, '
+                    'currentConfirmedCount Integer, statisticsData Text'),
+        'cn_city': ('UUID text unique Primary Key, Timestamp Integer, ISO_Date Text, '
+                    'ProvinceName Text, Province_EN Text, CityName Text, City_EN Text, '
+                    'currentConfirmedCount Integer, suspectedCount Integer, '
+                    'deadCount Integer, LocationID Integer, confirmedCount Integer, '
+                    'curedCount Integer'),
+        'jhu_git': ('UUID text unique Primary Key, Timestamp Integer, Country Text, '
+                    'Province Text, Confirmed Integer, Dead Integer, Cured Integer'),
+        'places' : ('OBJECTID Int UNIQUE Primary Key, ADMIN_TYPE Text, ADM2_CAP Text, '
+                    'ADM2_EN Text, ADM2_ZH Text, ADM2_PCODE Text, ADM1_EN Text, '
+                    'ADM1_ZH Text, ADM1_PCODE Text, ADM0_EN Text, ADM0_ZH Text, '
+                    'ADM0_PCODE Text'),
+        'files'  : ('filename Text, dateProcessed Text')
             }
 
     make_tables_from_dict(dbc, tabledefs)
@@ -78,7 +115,7 @@ def read_hksarg_pr():
                                  int(date_list[0]), int(date_list[3]), 
                                  int(date_list[4]))
         
-        sqlcmd = 'INSERT OR IGNORE INTO [hksarg_pr] (Timestamp, New, Total, Cured, Remain, Stable, Serious, Critical, Confirmed, Dead) Values (\"{}\", {});'.format(date, escaped)
+        sqlcmd = 'INSERT OR IGNORE INTO [hksarg] (Timestamp, New, Total, Cured, Remain, Stable, Serious, Critical, Confirmed, Dead) Values (\"{}\", {});'.format(date, escaped)
         dbdo(dbc, sqlcmd, VERBOSE)
     
     dbdo(dbc, 'COMMIT', VERBOSE)
@@ -100,7 +137,7 @@ def read_china_places():
         for component in components:
             values += r', "{}"'.format(component)
 
-        dbdo(dbc, 'INSERT into [china_places] ({}) Values ({})'.format(fields, values), VERBOSE)
+        dbdo(dbc, 'INSERT into [places] ({}) Values ({})'.format(fields, values), VERBOSE)
 
     dbdo(dbc, 'COMMIT', VERBOSE)
     return 1
@@ -112,9 +149,14 @@ def read_3g_dxy_cn_json():
     """
     files = os.listdir(DATADIR)
     areastat = re.compile(r'^([0-9]{8})_([0-9]{6})_getAreaStat.json$')
+    already_processed = array_from_query(dbc, 'select filename from files;') 
     for filename in files:
         match = areastat.match(filename)
-        if match:
+
+        if match and not(filename in already_processed):
+            dbdo(dbc, 'BEGIN', VERBOSE)
+            now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            dbdo(dbc, 'INSERT INTO [files] (Filename, DateProcessed) Values ("{}", "{}")'.format(filename, now), VERBOSE)
             date = match[1]
             time = match[2]
             timestamp = '{}{}'.format(date, time)
@@ -126,84 +168,116 @@ def read_3g_dxy_cn_json():
 
             print (len(areastats))
             # Walk the tree
-            dbdo(dbc, 'BEGIN', VERBOSE)
-            pfields = 'UUID, Timestamp, ISO_Date, Province_ZH, Province_EN, Confirmed, Suspected, Cured, Dead, Comment'
-            cfields = 'UUID, Timestamp, ISO_Date, Province_ZH, Province_EN, City_ZH, City_EN, Confirmed, Suspected, Cured, Dead'
+            pfields_base = 'UUID, Timestamp, ISO_Date, ProvinceName, Province_EN'
+            cfields_base = 'UUID, Timestamp, ISO_Date, ProvinceName, Province_EN, City_EN'
+            #cfields = 'UUID, Timestamp, ISO_Date, Province_ZH, Province_EN, CityEN, CityName, Confirmed, Suspected, Cured, Dead, AllConfirmed, LocationID'
             for province in areastats:
                 uuid = '{}_{}'.format(timestamp, province['provinceName'])
-                province_en = value_from_query(dbc, 'select distinct(ADM1_EN) from china_places where ADM1_ZH like \'{}%\';'.format(province['provinceName']))
-                values = '"{}", {}, "{}", "{}", "{}", {}, {}, {}, {}, "{}"'.format(
-                        uuid,
-                        int(timestamp),
-                        iso_date,
-                        province['provinceName'],
-                        province_en,
-                        province['confirmedCount'],
-                        province['suspectedCount'],
-                        province['curedCount'],
-                        province['deadCount'],
-                        province['comment'])
-                sql_cmd = 'INSERT into [3g_dxy_cn_province] ({}) Values ({})'.format(pfields, values)
-                dbdo(dbc, sql_cmd, VERBOSE)
+                province_en = value_from_query(dbc, 'select distinct(ADM1_EN) from places where ADM1_ZH like \'{}%\';'.format(province['provinceName']))
+                #print ('Province:', province.keys())
+                values = '"{}", {}, "{}", "{}", "{}"'.format(
+                            uuid, int(timestamp), iso_date, 
+                            province['provinceName'], province_en)
 
+                # Build up the string of Columns and values depending on what's in the JSON
+                pfields = pfields_base
+                print ('Province:', province.keys(), province)
+                for key in province.keys():
+                    if key != 'cities':
+                        pfields += ', {}'.format(key)
+                        if type(key) is str:
+                            values += ', "{}"'.format(province[key])
+                        else:
+                            values += ', {}'.format(province[key])
+
+                sql_cmd = 'INSERT into [cn_prov] ({}) Values ({})'.format(pfields, values)
+                dbdo(dbc, sql_cmd, VERBOSE)
                 #printlog (case_count)
                 
+                # Now do the same for every city in the province
                 for city in province['cities']:
                     uuid = '{}_{}_{}'.format(timestamp, 
                             province['provinceName'], 
                             city['cityName'])
-                    city_en = value_from_query(dbc, 'select distinct(ADM2_EN) from china_places where ADM2_ZH like \'{}%\';'.format(city['cityName']))
-                    values = '"{}", {}, "{}", "{}", "{}", "{}", "{}", {}, {}, {}, {}'.format(
-                            uuid,
-                            int(timestamp),
-                            iso_date,
-                            province['provinceName'],
-                            province_en,
-                            city['cityName'],
-                            city_en,
-                            city['confirmedCount'],
-                            city['suspectedCount'],
-                            city['curedCount'],
-                            city['deadCount'])
-                    sql_cmd = 'INSERT into [3g_dxy_cn_city] ({}) Values ({})'.format(cfields, values)
+                    city_en = value_from_query(dbc, 'select distinct(ADM2_EN) from [places] where ADM2_ZH like \'{}%\';'.format(city['cityName']))
+                    print ('City:', city.keys(), city)
+                
+                    # Build up the string of Columns and values depending on what's in the JSON
+                    cfields = cfields_base
+                    values = '"{}", {}, "{}", "{}", "{}", "{}"'.format(
+                            uuid, int(timestamp), iso_date, province['provinceName'],
+                            province_en, city_en)
+                    for key in city.keys():
+                        cfields += ', {}'.format(key)
+                        if type(city[key]) is str:
+                            values += ', "{}"'.format(city[key])
+                        else: 
+                            values += ', {}'.format(city[key])
+
+                    sql_cmd = 'INSERT into [cn_city] ({}) Values ({})'.format(cfields, values)
                     dbdo(dbc, sql_cmd, VERBOSE)
 
             dbdo(dbc, 'COMMIT', VERBOSE)
 
     return 1
+def read_jhu_data():
+
+    return 1
+def make_plot(province_en, dates, confirmed, dead, cured):
+    axis_range = [datetime.datetime(2020,1,29), datetime.datetime.now()]
+    fig, ax = plt.subplots()
+    fig.suptitle('NovelCoronaVirus Cases for {}'.format(province_en))
+    ax.set_title('Date')
+    ax.plot(dates, list(confirmed.values()), label='confirmed')
+    ax.plot(dates, list(dead.values()), label='dead')
+    ax.plot(dates, list(cured.values()), label='cured')
+    ax.set(xlabel='Date', xlim = axis_range, ylabel='Reported Cases')
+    ax.legend()
+    fig.savefig('plots/{}.png'.format(province_en), format = 'png')
+
+    plt.close()
+    return 0;
 
 def make_plots():
     """ 
     Make a plot by province:
     """
-    axis_range = [datetime.datetime(2020,1,29), datetime.datetime.now()]
-    provinces = array_from_query(dbc, 'select distinct(province_ZH) from [3g_dxy_cn_province];')
+    provinces = array_from_query(dbc, 'select distinct(provinceName) from [cn_prov];')
+    china_total_conf = {}
+    china_total_date = {}
+    china_total_cure = {}
+    china_total_dead = {}
+    chinadates = []
     for province in provinces:
         province_en = value_from_query(dbc, 'select distinct(ADM1_EN) from china_places where ADM1_ZH like \'{}%\';'.format(province))
-        confirmed = dict_from_query(dbc, 'select iso_date, confirmed from [3g_dxy_cn_province] where province_ZH like \'{}\' order by timestamp;'.format(province))
-        dead = dict_from_query(dbc, 'select iso_date, dead from [3g_dxy_cn_province] where province_ZH like \'{}\' order by timestamp;'.format(province))
-        cured = dict_from_query(dbc, 'select iso_date, cured from [3g_dxy_cn_province] where province_ZH like \'{}\' order by timestamp;'.format(province))
-
         print(province, province_en)
+
+        confirmed = dict_from_query(dbc, 'select iso_date, confirmedCount from [cn_prov] where provinceName like \'{}\' order by timestamp;'.format(province))
+        dead = dict_from_query(dbc, 'select iso_date, deadCount from [cn_prov] where provinceName like \'{}\' order by timestamp;'.format(province))
+        cured = dict_from_query(dbc, 'select iso_date, curedCount from [cn_prov] where provinceName like \'{}\' order by timestamp;'.format(province))
         dates = []
-        for date_str in confirmed.keys():
-            date = datetime.datetime.strptime(date_str, '%Y-%m-%d %H:%M:%S')
-            dates.append(date)
 
-        #print (dates)
-        fig, ax = plt.subplots()
-        fig.suptitle('NovelCoronaVirus Cases for {}'.format(province))
-        ax.set_title('Date')
-        ax.plot(dates, confirmed.values(), label='confirmed')
-        ax.plot(dates, dead.values(), label='dead')
-        ax.plot(dates, cured.values(), label='cured')
-        ax.set(xlabel='Date', xlim = axis_range, ylabel='Reported Cases')
-        ax.legend()
-        fig.savefig('plots/{}.png'.format(province_en), format = 'png')
+        #add each province to the China total
+        for key in confirmed.keys():
+            #print (key, confirmed[key], cured[key], dead[key], '\n')
+            china_total_conf[key] = china_total_conf.setdefault(key, 0) + confirmed[key]
+            china_total_cure[key] = china_total_cure.setdefault(key, 0) + cured[key]
+            china_total_dead[key] = china_total_dead.setdefault(key, 0) + dead[key]
+            china_total_date[key] = china_total_date.setdefault(key, 0) + 1
+            isodate = datetime.datetime.strptime(key, '%Y-%m-%d %H:%M:%S')
+            dates.append(isodate)
+            if isodate not in chinadates:
+                chinadates.append(isodate)
 
-        plt.close()
-        exit()
+            #time = datetime.datetime.strptime(date_str, '%H:%M:%S')
 
+        #print (type(dates), dates)
+        make_plot(province_en, dates, confirmed, dead, cured)
+
+
+    #print (type(chinadates), chinadates)
+
+    make_plot('GreaterChina', chinadates, china_total_conf, china_total_dead, china_total_cure)
     return 0
 
 
@@ -218,7 +292,9 @@ def main():
         read_hksarg_pr()
         read_china_places()
         read_3g_dxy_cn_json()
+        read_jhu_data()
     else:
+        read_3g_dxy_cn_json()
         make_plots()
 
     return 0
@@ -227,6 +303,11 @@ if __name__ == '__main__':
     VERBOSE = 1
     FIRSTRUN = 0
     DATADIR = '01_download_data'
+    for arg in sys.argv:
+        if arg == 'VERBOSE':
+            VERBOSE = 1
+        if arg == 'FIRSTRUN':
+            FIRSTRUN = 1
     
     db_connect = sqlite3.connect('ncorv2019.sqlite')
     dbc = db_connect.cursor()
