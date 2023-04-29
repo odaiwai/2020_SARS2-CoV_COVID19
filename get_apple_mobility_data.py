b373820fd92817d7ab3e276c23c94d4b46828861
Dave O'Brien <github@diaspoir.net>
2023-04-29 17:27:10 +0800

    tidied up the apple mobility getter to satisfy the linter.

--- get_apple_mobility_data.py
+++ get_apple_mobility_data.py
@@ -1,37 +1,38 @@
 #!/usr/bin/env python3
 """ Download and plot the Apple Mobility Data"
 """
-import os
 import json
 import requests
-import matplotlib.pyplot as plt
-import matplotlib as mpl
-import matplotlib.dates as mdates # for date formatting
+
 
 def main():
-    # get the index json
-    uri_list = ['www.apple.com' , 'local.apple.com' , 'covid19-static.cdn-apple.com']
-    cdn_uri = r'https://{}'.format(uri_list[2])
+    """
+    get the index json
+    """
+    uri_list = ['www.apple.com',
+                'local.apple.com',
+                'covid19-static.cdn-apple.com']
+    cdn_uri = f'https://{uri_list[2]}'
     idx_path = r'/covid19-mobility-data/current/v1/index.json'
-    response = requests.get('{}{}'.format(cdn_uri, idx_path))
+    response = requests.get(f'{cdn_uri}{idx_path}')
     # Walk it and get the mobility files
     idx_json = json.loads(response.text)
-    basePath = idx_json['basePath']
+    base_path = idx_json['basePath']
     regions = idx_json['regions']
     for locale in regions.keys():
-        jsonPath = idx_json['regions'][locale]['jsonPath']
-        csvPath =  idx_json['regions'][locale]['csvPath']
-        version =  idx_json['regions'][locale]['mobilityDataVersion']
-        print (idx_json, '\n', locale, jsonPath, csvPath, version)
+        json_path = idx_json['regions'][locale]['jsonPath']
+        csv_path = idx_json['regions'][locale]['csvPath']
+        version = idx_json['regions'][locale]['mobilityDataVersion']
+        print(idx_json, '\n', locale, json_path, csv_path, version)
         # Download them and build some graphs
-        response = requests.get('{}/{}/{}'.format(cdn_uri, basePath, jsonPath))
-        print(response)#, response.text)
+        response = requests.get(f'{cdn_uri}/{base_path}/{json_path}')
+        print(response)
         mob_data = json.loads(response.text)
         places = {}
         names = {}
         titles = {}
         for place in mob_data['data']:
-            print (place)
+            print(place)
             places[place] = places.setdefault(place, 0) + 1
             place_data = mob_data['data'][place]
             for dataset in place_data:
@@ -42,11 +43,12 @@ def main():
                     if key == 'title':
                         titles[dataset[key]] = titles.setdefault(dataset[key], 0) + 1
                     print(place, ':', key, ':', dataset[key])
-        
-        print (places)
-        print (names)
-        print (titles)
-    return None
+
+        print(places)
+        print(names)
+        print(titles)
+
+
 if __name__ == '__main__':
     LOCALE = 'en-us'
     main()
